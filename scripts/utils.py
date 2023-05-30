329f9ee31578512e9f773c2b3c8ed70f83221288
Ryan <ryan@maudruz.ch>
2023-05-30 16:46:27 +0200

    continued

--- scripts/utils.py
+++ scripts/utils.py
@@ -12,6 +12,7 @@ import torch
 import numpy as np
 import os
 import uuid
+import json
 from transformers import set_seed
 
 
@@ -93,3 +94,25 @@ def get_uuid_for_imgs(img_list):
     img_list.sort()
     # Only the exact same list of images will lead to the same UUID.
     return str(uuid.uuid3(uuid.NAMESPACE_DNS, ''.join(img_list)))
+
+
+def get_samples_sqa(sample_idxs_path='../data/scienceqa/sample_idxs.json'):
+    """
+    Saves a list of sample indices for each task in ScienceQA dataset.
+    """
+    sqa_samples = {
+        'cot': [
+            68, 90, 122, 142, 155, 167, 177, 191, 202, 227, 234, 236, 254, 325, 340, 352, 364, 369, 370, 372, 374, 376,
+            426, 429, 430, 432, 433, 448, 449, 461, 472, 473, 474, 475, 501, 502, 506, 513, 523, 529, 530, 534, 544,
+            553, 557, 559, 570, 574, 575, 601, 602, 606, 607, 609
+        ],
+        'vqa': [
+            135, 140, 148, 155, 177, 202, 215, 223, 227, 234, 236, 237, 254, 257, 301, 307, 310, 311, 316, 319, 320,
+            324, 325, 327, 331, 334, 342, 348, 352, 360, 363, 364, 368, 369, 372, 400, 406, 426, 427, 433, 436, 449,
+            461, 470, 491, 494, 506, 507, 508, 509, 513, 523, 527, 530, 540, 545, 550, 557, 558, 568, 574, 583, 585,
+            588, 603, 605
+        ]
+    }
+    # save samples to file
+    with open(sample_idxs_path, 'w') as f:
+        json.dump(sqa_samples, f)
